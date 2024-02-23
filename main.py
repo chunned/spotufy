95c68e7245f1e64ad0ab6cb7f1a521859eed898e
chunned <72193737+chunned@users.noreply.github.com>
2024-02-23 15:37:09 -0500

    Add searchArtists() function

--- main.py
+++ main.py
@@ -35,3 +35,58 @@ def requestApiToken():
         print('No access token found in API response. Ensure your CLIENT_ID and CLIENT_SECRET are correct.')
         exit(1)
 
+
+def searchArtists(apiToken, artist):
+    headers = {"Authorization": f"Bearer {apiToken}"}
+    # URL encode artist string to ensure request executes properly
+    query = urllib.parse.quote(artist)
+    # Construct the query URL
+    url = f"{APIURL}/search?q={query}&type=artist&limit=5"
+
+    # Send the query - will return a list of matching artists
+    try:
+        response = requests.get(url, headers=headers)
+        # If HTTP status code in response is anything other than 200 OK, raise an error
+        response.raise_for_status()
+        response = response.json()
+        # Check if any artists were found during search
+        if response['artists']['total'] == 0:
+            raise ValueError("No search results found!")
+    except requests.HTTPError as e:   # Catch non-200 status codes
+        print(f"Error in API response code: {e}")
+        exit(1)
+    except ValueError as e:
+        print(f"Error in search results: {e}")
+        exit(1)
+
+    # Construct search result output
+    artists = ['']  # Will hold the artist results - insert one null value at index 0 for easier array access
+    for artist in response['artists']['items']:
+        artistResult = {
+            "name": artist["name"],
+            "url": artist["external_urls"]["spotify"],
+            "followers": artist["followers"]["total"],
+            "popularity": artist["popularity"],
+            "genres": artist["genres"]
+        }
+        try:
+            artistResult['imageUrl'] = artist["images"][0]["url"]
+        except IndexError:
+            artistResult['imageUrl'] = "Image not found"
+
+        artists.append(artistResult)
+
+    print("Displaying search results. Please select the matching artist.")
+    # Iterate through artists, prompt user to select the correct one
+    for i in range(1, len(artists)):
+        print(f"""RESULT #{i}
+NAME: {artists[i]['name']}
+URL: {artists[i]['url']}
+FOLLOWERS: {artists[i]['followers']}
+GENRE(s): {artists[i]['genres']}
+PHOTO: {artists[i]['imageUrl']}
+
+---
+""")
+
+
