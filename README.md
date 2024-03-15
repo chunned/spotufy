63833b5a5b42897099a8d99954c61b8555e26c2a
chunned <72193737+chunned@users.noreply.github.com>
2024-03-14 20:10:48 -0400

    Misc formatting, PEP8, HTML template, README changes

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # SpOTUfy
+![SpOTUfy is an application for the purpose of providing greater value to the everyday Spotify user who wants to get more out of their experience. 
+](static/homepage.gif)
 
-SpOTUfy is an application for the purpose of providing greater value to the everyday Spotify user who wants to get more out of their experience. 
 
-# Features Roadmap
-## Complete
-- Search song details
-- Get playlist of artist's top tracks
-- 
-## To Do
-- Search artist information
-- Show recommended songs based on user activity
-- Get song lyrics
-- Get related artists
-- Search artist discography
+# Features 
 ## Backlog
 - Get artist new/upcoming releases
 - On demand "Daily Mixes"
 - Graph visualization of artist relations
+
+## Available
+- Search artist information
+- Get playlist of artist's top tracks
+- Search song details
+- Get recommendations based on a song
+- Get recommended songs based on user activity
+- Get artists related to a given input artist
+- Get song lyrics
+- Search artist discography
+
+
+
--- test_unittests.py
+++ test_unittests.py
@@ -1,73 +1,73 @@
 import unittest
-from main import *
+from spotufy import *
 
 token = request_api_token()
 
 class make_api_call_test(unittest.TestCase):
-    '''Test module to test API call function in `main.py`'''
+    """Test module to test API call function in `spotufy.py`"""
     def test_valid_return(self):
-        '''Function should return a JSON object if given valid input'''
+        """Function should return a JSON object if given valid input"""
         url = "https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl"
         headers = {"Authorization": f"Bearer {token}"}
         self.assertTrue(type(make_api_call(url, "GET", headers)) == type({}))
     def test_HTTP_error_return(self):
-        '''Function should return None if an HTTP status code is raised'''
+        """Function should return None if an HTTP status code is raised"""
         url = "https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl"
         # Invalid token, should raise a 401 error and return None
         headers = {"Authorization": f"Bearer wefoijweoijsxoijwed"}
         self.assertTrue(None==make_api_call(url, "GET", headers))
     def test_invalid_schema_return(self):
-        '''Function should return None if the schema of the URL is wrong/missing'''
+        """Function should return None if the schema of the URL is wrong/missing"""
         # URL missing schema, should raise exception and return None
         url = "api.spotify.com/v1/track/11dFghVXANMlKmJXsNCbNl"
         headers = {"Authorization": f"Bearer {token}"}
         self.assertTrue(None==make_api_call(url, "GET", headers))
 
 class request_api_token_test(unittest.TestCase):
-    '''Test module to test request API token function in `main.py`'''
+    """Test module to test request API token function in `spotufy.py`"""
     def test_token_received(self):
-        '''API token string should be returned and not be None'''
+        """API token string should be returned and not be None"""
         self.assertTrue(None!=request_api_token())
 
 class search_artists_test(unittest.TestCase):
-    '''Test module to test search artists function in `main.py'''
+    """Test module to test search artists function in `spotufy.py"""
     def test_valid_return(self):
-        '''Function should return a non-empty list if given good input'''
+        """Function should return a non-empty list if given good input"""
         self.assertTrue([]!=search_artists(token, "Al Green"))
     def test_invalid_return(self):
-        '''Function should return None if given bad input (no matching artist from search)'''
+        """Function should return None if given bad input (no matching artist from search)"""
         self.assertTrue(None==search_artists(token, "sdfouhxiuheiuwer"))
 
 class search_song_details_test(unittest.TestCase):
-    '''Test module to test search song details function in `main.py`'''
+    """Test module to test search song details function in `spotufy.py`"""
     def test_blank_input(self):
-        '''Function should return None if given empty input'''
+        """Function should return None if given empty input"""
         self.assertTrue(None==search_song_details(token, "", ""))
     def test_no_match(self):
-        '''Function should return None if there is no matching result from API'''
+        """Function should return None if there is no matching result from API"""
         self.assertTrue(None==search_song_details(token, "Banana Pop Bozo", "Frisbee Cube Clock"))
     def test_valid_return(self):
-        '''Function should return a dictionary of track information if given good input'''
+        """Function should return a dictionary of track information if given good input"""
         self.assertTrue(type(search_song_details(token, "Resonance", "Home")) == type({}))
 
 class parse_input_test(unittest.TestCase):
-    '''Test module to test parse input function in `main.py`'''
+    """Test module to test parse input function in `spotufy.py`"""
     def test_valid_return(self):
-        '''Function should return a non-empty string when called'''
+        """Function should return a non-empty string when called"""
         self.assertTrue(type(parse_input("ABxcII29238JJb"))==str and None!=parse_input("ABxcII29238JJb"))
 
 class get_artist_releases_test(unittest.TestCase):
-    '''Test module to test get artist releases function in `main.py`'''
+    """Test module to test get artist releases function in `spotufy.py`"""
     def test_invalid_artist_input(self):
-        '''Function should return None when not given an artist dictionary item as returned by `searchArtist()`'''
+        """Function should return None when not given an artist dictionary item as returned by `searchArtist()`"""
         self.assertTrue(None==get_artist_releases(token, "Al Green"))
     def test_valid_input(self):
-        '''Function should return an array of release items if given valid input and artist has releases'''
+        """Function should return an array of release items if given valid input and artist has releases"""
         self.assertTrue(type(get_artist_releases(token, search_artists(token, "Al Green")))==type([]))
 
 # # Unit test template
 # class name_test(unittest.TestCase):
-#     '''Test module to test __ function in `main.py`'''
+#     '''Test module to test __ function in `spotufy.py`'''
 #     def test_name(self):
 #         ''''''
 #         self.assertTrue()
